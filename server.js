9272cbaaada2d8a87642c06a845c7e31e59084f7
Subham Das <subhamdassneha@gmail.com>
2023-02-02 00:04:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,8 +30,9 @@ app.use(cookieParser());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/root'))
-app.use('/users', require('./routes/userRoutes'))
+app.use('/', require('./routes/root'));
+app.use('/users', require('./routes/userRoutes'));
+app.use('/notes', require('./routes/noteRoutes'));
 
 
 //Handling 404 requests
